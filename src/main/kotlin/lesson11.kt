d90ca7e9650cc4861446c8a868eb44ce805a9f08
praban <pbanawalkar2@gmail.com>
2023-06-04 12:53:29 +0530

    kotlin basics section completed

---
+++ src/main/kotlin/lesson11.kt
@@ -0,0 +1,27 @@
+fun main(){
+//    val elemnts = listOf<String>("A","B")
+//    for (e in elemnts){
+//        println(e)
+//    }
+//
+//    for (number in 1..10){
+//        println(number)
+//    }
+//
+//    for (number in 1 until 10){
+//        println(number)
+//    }
+//    showRange(range = 1..10)
+//    showRange(range = 3 downTo 1)
+//    showRange(range = 0..10 step 2)
+//    showRange(range = 9..0 step 3)
+    repeat(times = 3){
+
+    }
+}
+fun showRange(range: IntProgression){
+    for (number in range){
+        println("Current number:$number")
+    }
+    println("// $range")
+}
